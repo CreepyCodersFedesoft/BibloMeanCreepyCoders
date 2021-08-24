cea286a85305d6a36914263ed0fb3a442be38829
Diego R <alejorvela@gmail.com>
2021-08-24 10:45:39 -0500

    list and create books added

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -1,8 +1,30 @@
 const Book = require("../models/book");
+const Library = require("../models/library")
 
-const registerBook = async (req, res) => {};
-//se podrá filtrar por nombre del libro
-const listBook = async (req, res) => {};
+const registerBook = async (req, res) => {
+  if(!req.body.name || !req.body.author )//|| !req.body.libraryId
+    return res.status(400).send("Error: There'r empty fields");
+
+  //validar que la biblioteca exista
+  let existingLibrary = await Library.findById({_id: req.body.libraryId});
+  if(!existingLibrary) return res.status(400).send("Error: Library doesn't exist");
+
+  let book = new Book({
+    name: req.body.name,
+    author: req.body.author,
+    libraryId: req.body.libraryId,
+  });
+
+  let result = await book.save();
+  if(!result) return res.status(400).send("Error: Error to save data");
+  return res.status(200).send({ result });
+};
+
+const listBook = async (req, res) => {
+  let book = await Book.find({ name: new RegExp(req.params["name"], "i")}).populate('libraryId').exec();
+  if(!book || book.length === 0) return res.status(400).send("Error: book  doesn't exist");
+  return res.status(200).send({ book });
+};
 //update podrá modificar la biblioteca a la que pertenece el libro
 const updateBook = async (req, res) => {};
 //delete cambia el status de un libro a false
--- backend/routes/book.js
+++ backend/routes/book.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const BookController = require('../controllers/book')
 
 router.post('/registerBook', BookController.registerBook);
-router.get('/listBook/:name?', BookController.listBook);
+router.get('/listBook/:name?/', BookController.listBook);
 router.put('/updateBook', BookController.updateBook);
 router.put('/deleteBook', BookController.deleteBook);
 
